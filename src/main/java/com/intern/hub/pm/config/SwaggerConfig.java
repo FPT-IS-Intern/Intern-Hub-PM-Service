a489ad30709579d5311b9ebeb339fe8d581d7c4c
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-12 22:45:45 +0700

    refactor(swagger): Simplify Swagger security scheme configuration

--- src/main/java/com/intern/hub/pm/config/SwaggerConfig.java
+++ src/main/java/com/intern/hub/pm/config/SwaggerConfig.java
@@ -1,8 +1,6 @@
 package com.intern.hub.pm.config;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OpenApiCustomizer;
@@ -18,22 +16,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenApiCustomizer openApiCustomizer() {
-        SecurityScheme bearerAuthScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        SecurityScheme internalApiKeyScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.APIKEY)
-                .in(SecurityScheme.In.HEADER)
-                .name("X-Internal-Secret");
-
         return openApi -> openApi
                 .addServersItem(new Server().url(gatewayUrl + "/api"))
                 .components((
                         openApi.getComponents() == null ? new Components() : openApi.getComponents())
-                        .addSecuritySchemes("Bearer", bearerAuthScheme)
-                        .addSecuritySchemes("InternalAPIKey", internalApiKeyScheme)
+                        .addSecuritySchemes(
+                                "Bearer",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))
+                        .addSecuritySchemes(
+                                "InternalAPIKey",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-Internal-Secret"))
                 );
     }
 
